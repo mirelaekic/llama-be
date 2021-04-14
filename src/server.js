b3d568758107966e7c3539547cbee917748f0865
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-04-14 14:38:14 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -44,6 +44,11 @@ server.use(express.json());
 //   })
 // );
 server.use(cors({credentials: true, origin: process.env.FE_URL}));
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
 const port = process.env.PORT;
 const staticFolderPath = join(__dirname, "../public");
 
